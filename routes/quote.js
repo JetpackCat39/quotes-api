3a2781753be448560a5aeac80bbe1f4f9d1cd86f
Evan Thurston <evanthurston@gmail.com>
2016-12-01 00:06:14 -0800

    connect to mongo. still not using db.

--- routes/quote.js
+++ routes/quote.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+var MongoClient = require('mongodb').MongoClient, assert = require('assert');
+
+var url = 'mongodb://heroku_hwn3xb29:d2l1apo2soe77k7ekhs46r94rn@ds159507.mlab.com:59507/heroku_hwn3xb29';
+
 router.get('/', function(req, res, next) {
-    res.send("cool things");
+    MongoClient.connect(url, function(err, db) {
+        assert.equal(null, err);
+        res.send("Connected successfully to server");
+
+        db.close();
+    });
 });
 
 module.exports = router;
