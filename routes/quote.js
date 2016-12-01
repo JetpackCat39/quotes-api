47623ac2ae88d6e5a5afed4597e1dd080bf0f463
Evan Thurston <evanthurston@gmail.com>
2016-12-01 00:46:51 -0800

    i think this works

--- routes/quote.js
+++ routes/quote.js
@@ -5,10 +5,10 @@ var MongoClient = require('mongodb').MongoClient, assert = require('assert');
 
 var url = 'mongodb://heroku_hwn3xb29:d2l1apo2soe77k7ekhs46r94rn@ds159507.mlab.com:59507/heroku_hwn3xb29';
 
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
     MongoClient.connect(url, function(err, db) {
         assert.equal(null, err);
-        res.send("Connected successfully to server");
+        res.send(db.quotes.find());
 
         db.close();
     });
