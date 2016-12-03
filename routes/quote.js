553f230610c7ca5da66da5e2c9854fb9b977d90c
Evan Thurston <evanthurston@gmail.com>
2016-12-02 23:57:14 -0800

    more testing

--- routes/quote.js
+++ routes/quote.js
@@ -1,22 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
-var MongoClient = require('mongodb').MongoClient, assert = require('assert');
+var MongoClient = require('mongodb').MongoClient;
 
 var url = 'mongodb://heroku_hwn3xb29:d2l1apo2soe77k7ekhs46r94rn@ds159507.mlab.com:59507/heroku_hwn3xb29';
 
 router.get('/', function(req, res) {
     MongoClient.connect(url, function(err, db) {
-        assert.equal(null, err);
+        if (err) {
+            console.log('Unable to connect to the mongoDB server. Error:', err);
+        } else {
+            //HURRAY!! We are connected. :)
+            console.log('Connection established to', url);
 
-        var col = db.collection("quotes");
+            // var col = db.collection("quotes");
+            //
+            // col.find({}).toArray(function(err, docs) {
+            //     assert.equal(err, null);
+            //     res.send(docs);
+            // });
 
-        col.find({}).toArray(function(err, docs) {
-            assert.equal(err, null);
-            res.send(docs);
-        });
-            
-        db.close();
+            db.close();
+        }
     });
 });
 
